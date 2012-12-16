48848cb5d7d026e2132a6ff160ca1754ef53f518
Xin <gmacg.uk@gmail.com>
2012-12-16 20:02:14 +0000

    Added new insight.js, function not yet defined.

---
+++ insight.js
@@ -0,0 +1,15 @@
+//INSIGHT
+
+(function() {
+
+	$(document).ready( function() {
+		$('#rings').find('input:enabled')
+			.change(setInsight);
+		$('#skills').find('input:enabled')
+			.change(setInsight);
+	});
+
+	var setInsight = function(e) {
+		
+	}
+})();
